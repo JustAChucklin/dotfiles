93072e0c1f74d04adafc6f2395d7aff153f04e19
Chuck Blackard <robbyblackard@gmail.com>
2026-08-23 19:25:46 -0500

    feat: adding mappings in neovim

--- linux/.config/nvim/lua/mappings.lua
+++ linux/.config/nvim/lua/mappings.lua
@@ -6,5 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "increase window width" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
